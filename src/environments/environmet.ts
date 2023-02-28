3bd0b2945c6ea0d3a5925ed8e6edbf875c33255d
Juan Pablo Patiño <82609508+JuanFlorez1326@users.noreply.github.com>
2023-02-28 18:44:18 -0500

    DELETE: Removed the angular flex layout from the entire app.

--- src/environments/environmet.ts
+++ src/environments/environmet.ts
@@ -4,11 +4,3 @@ export const environment = {
     key: '31461a5f2bc8644aa2139447f3ab4eae',
     hash: '0a92707e499f3bb11f4e5681c225350b',
 };
-
-//ts: '1',
-//orderBy: '-focDate',
-//orderByName: 'name',
-//orderByStory: '-id',
-//limit: '6',
-//limitStories: '5',
-//limitCharacters: '29'
