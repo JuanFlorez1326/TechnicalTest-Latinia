7b581df70a79bf9a73e6cd170832c1b23150f7b5
Juan Pablo Patiño <82609508+JuanFlorez1326@users.noreply.github.com>
2023-03-03 16:35:51 -0500

    ADD: The rendering of all the cards in the home view was done through ngrx

--- src/app/marvel/components/all-characters/all-characters.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-all-characters',
-  templateUrl: './all-characters.component.html'
-})
-export class AllCharactersComponent {}
--- src/app/marvel/state/reducers/character.reducers.ts
+++ src/app/marvel/state/reducers/character.reducers.ts
@@ -1,15 +1,12 @@
 import { createReducer, on } from '@ngrx/store';
 import { CharacterState } from '../../interfaces/characters.state';
-import { loadCharacters, loadCharactersSuccess } from '../actions/character.actions';
+import { loadCharactersSuccess } from '../actions/character.actions';
 
 export const initialState: CharacterState = { loading: false, characters: [] }; 
 
 export const characterReducer = createReducer(
     initialState,
-    on( loadCharacters, ( state ) => {
-        return { ...state, loading: true };
-    }),
     on( loadCharactersSuccess, ( state, { characters } ) => {
-        return { ...state, loading: false, characters: [ ...characters ] };
-    })
+        return { ...state, characters: [ ...characters ] };
+    }),
 );
